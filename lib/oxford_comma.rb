0cc088f52c426a869337b178823d63ca62520a86
Ola Enol <eniolar10@gmail.com>
2020-01-31 15:30:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,13 +7,5 @@ def oxford_comma(array)
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
     
- #if array.length == 2
-  # array[-2] << "and"
-  # array.join 
-#elseif array.length == 1
-#  array.join 
-# elseif array.length > 2
- #  array[-1].prepend "and"
- #  array.join (",")
- end
+  end
 end
